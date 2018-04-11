00ecd78558cf0613bf9bc28cd85fc31f07537dc1
Seth Caparelli <sethcaparelli@gmail.com>
2018-04-11 10:20:49 -0600

    basic-styling done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,10 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <img id="sc-logo" src="/assets/icons/personal-logo.png" alt="personal-icon"/>
-                <h1>Seth Caparelli</h1>
+                <div id="logo-container">
+                    <img id="sc-logo" src="/assets/icons/personal-logo.png" alt="personal-icon"/>
+                    <h1>Seth Caparelli</h1>
+                </div>
             </header>
         )
     }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,13 +4,36 @@ class Menu extends Component {
     render(){
         return(
             <div id="menu">
-                <button onClick={this.props.getCactus}>Cactus^3</button>
-                <button onClick={this.props.getTree}>Reclaim Tree</button>
-                <button onClick={this.props.getSculpture}>Sculpture</button>
-                <button onClick={this.props.getVessel}>Vessel</button>
-                <button onClick={this.props.getPainting}>Painting</button>
-                <button onClick={this.props.getAdvertising}>Advertising</button>
-                <button onClick={this.props.getDesign}>Graphic Design</button>
+                <div className="icon-container" id="cactus-container">
+                    <img className="icon-image" id="cactus-icon" src="/assets/icons/cactus-icon.png" alt="/assets/icons/camera-icon.png" onClick={this.props.getCactus}/>
+                    <small>Cactus</small>
+                </div>
+                <div className="icon-container" id="tree-container">
+                    <img className="icon-image" id="tree-icon" src="/assets/icons/tree-icon.png" alt="/assets/icons/camera-icon.png" onClick={this.props.getTree}/>
+                    <small>Reclaim Tree</small>
+                </div>
+                <div className="icon-container" id="sculpture-container">
+                    <img className="icon-image" id="sculpture-icon" src="/assets/icons/sculpture-icon.png" alt="/assets/icons/camera-icon.png" onClick={this.props.getSculpture}/>
+                    <small>Sculpture</small>
+                </div>
+                <div className="icon-container" id="vessel-container">
+                    <img className="icon-image" id="vessel-icon" src="/assets/icons/vessel-icon.png" alt="/assets/icons/camera-icon.png" onClick={this.props.getVessel}/>
+                    <div>
+                        <small>Vessel</small>
+                    </div>
+                </div>
+                <div className="icon-container" id="painting-container">
+                    <img className="icon-image" id="painting-icon" src="/assets/icons/painting-icon.png" alt="/assets/icons/camera-icon.png" onClick={this.props.getPainting}/>
+                    <small>Painting</small>
+                </div>
+                <div className="icon-container" id="advertising-container">
+                    <img className="icon-image" id="advertising-icon" src="/assets/icons/advertising-icon.png" alt="/assets/icons/camera-icon.png" onClick={this.props.getAdvertising}/>
+                    <small>Advertising</small>
+                </div>
+                <div className="icon-container" id="design-container">
+                    <img className="icon-image" id="design-icon" src="/assets/icons/design-icon.png" alt="/assets/icons/camera-icon.png" onClick={this.props.getDesign}/>
+                    <small>Graphic Design</small>
+                </div>
             </div>
         )
     }
